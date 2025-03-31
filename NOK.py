46605dc276004f40230d411b7f1e82d2c96326cb
Dethkalleon <soldatkin2013@yandex.ru>
2025-03-31 20:35:04 +0300

    Изменение логики игры

--- NOK.py
+++ NOK.py
@@ -10,21 +10,22 @@ def play_game():
     print(f"Hello, {name}!\n")
     print("Find the smallest common multiple of given numbers.\n")
     
-    for _ in range(3):
+    while True:
         numbers = [random.randint(1, 100) for _ in range(3)]
         correct_answer = lcm(*numbers)
         
         print(f"Question: {numbers[0]} {numbers[1]} {numbers[2]}")
         user_answer = input("Your answer: ")
         
+        if user_answer.lower() == 'exit':
+            print(f"Goodbye, {name}!")
+            break
+        
         if user_answer.isdigit() and int(user_answer) == correct_answer:
             print("Correct!\n")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-    
-    print(f"Congratulations, {name}!")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n")
+            print("Let's try again!")
 
 if __name__ == "__main__":
     play_game()
