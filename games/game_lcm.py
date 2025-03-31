fdad1286d85bfafc7d6bb592450bb5b226fb435c
Dethkalleon <soldatkin2013@yandex.ru>
2025-03-31 22:59:12 +0300

    Небольшие исправления

--- games/game_lcm.py
+++ games/game_lcm.py
@@ -2,7 +2,7 @@ import random
 import math
 
 def generate_round():
-    numbers = [random.randint(1, 100) for _ in range(3)]
+    numbers = [random.randint(1, 10) for _ in range(3)]
     correct_answer = math.lcm(*numbers)
     question = " ".join(map(str, numbers))
     return question, correct_answer
